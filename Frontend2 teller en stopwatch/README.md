a6d97924164eacd6d02eb2c93e27189b68d73b0e
Glinshi <glinshi2@hotmail.com>
2025-03-20 10:22:31 +0100

    Add files via upload

---
+++ Frontend2 teller en stopwatch/README.md
@@ -0,0 +1,23 @@
+Counterwatch
+Counterwatch is a simple web app with two main features: a counter and a stopwatch.
+
+Features
+Counter:
+*Increment and decrement values between -10 and 10.
+*Resets to 0 when limits are reached.
+Stopwatch:
+*Start and stop a stopwatch tracking minutes and seconds.
+
+Technologies
+*HTML
+*CSS
+*JavaScript
+*Google Fonts (Open Sans)
+
+Usage
+*Counter: Click "+" to increment, "-" to decrement.
+*Stopwatch: Click "START" to begin, "STOP" to halt.
+
+How It Works
+The Counter starts at 0 and adjusts within the range of -10 to 10.
+The Stopwatch tracks time in minutes and seconds.
---
+++ Frontend2 teller en stopwatch/js/main.js
@@ -0,0 +1,58 @@
+// Counter
+var count = 0;
+var counter = document.getElementById("counter");
+counter.innerText = count
+
+var redButton = document.getElementById("button--red");
+redButton.onclick = function () {
+    if (count > -10)
+        count -= 1;
+    else {
+        count = 0
+    }
+    counter.innerText = count;
+}
+
+var greenButton = document.getElementById("button--green");
+greenButton.onclick = function () {
+    if (count < 10)
+        count += 1;
+    else {
+        count = 0
+    }
+    counter.innerText = count;
+}
+
+//stopwatch
+var minutes = 0;
+var seconds = 0;
+var everySecond
+var watch = document.getElementById("stopwatch");
+watch.innerText = minutes + ":0" + seconds;
+
+var start = document.getElementById("button--start")
+start.onclick = function(){
+    
+// elke seconde iets uitvoeren
+everySecond = setInterval(function () {
+    seconds += 1;
+    if(seconds > 60){
+        seconds = 0;
+        minutes += 1;
+    }
+    if (seconds > 9) {
+        watch.innerText = minutes + ":" + seconds;
+    }
+    else {
+        watch.innerText = minutes + ":0" + seconds;
+    }
+
+
+
+}, 1000)
+}
+
+var stop = document.getElementById("button--stop");
+stop.onclick = function() {
+    clearInterval(everySecond)
+}
